bde94a7e288c6b27bc0f17045466b71f6eb85491
FX-HAO <250491187@qq.com>
2014-04-21 21:53:58 +0800

    version 1.0

---
+++ GUI for crawler of jxust.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+from tkinter import *
+from py.custom.crawlerJXUST import GPA
+from tkinter import ttk
+
+class GUIGPA(object):
+    def __init__(self,initdir=None):
+        self.root=Tk()
+        self.root.title('Student GPA')
+        tip=Label(self.root,text='Please enter ur student number and password!')
+        tip.pack()
+
+        self.snofm=Frame(self.root)
+        self.snolabel=Label(self.snofm,text='Sno: ')
+        self.sno=StringVar(self.snofm)
+        self.snolabel.pack(side=LEFT)
+        self.snoarea = Entry(self.snofm, width=20,textvariable=self.sno)
+        self.snoarea.pack(side=RIGHT)
+        self.snofm.pack()
+
+        self.pwdfm=Frame(self.root)
+        self.pwdlabel=Label(self.pwdfm,text='Pwd: ')
+        self.pwdlabel.pack(side=LEFT)
+        self.pwd=StringVar(self.pwdfm)
+        self.pwdarea=Entry(self.pwdfm,width=20,textvariable=self.pwd)
+        self.pwdarea.pack(side=RIGHT)
+        self.pwdfm.pack()
+        
+        self.submit=Button(self.root,text='submit',command=self.getGPA,bg='black',fg='red')
+        self.submit.pack(expand=1)
+        
+        self.root.mainloop()
+
+    def getGPA(self):
+        if hasattr(self,'info'):
+            self.info.destroy()
+        student=GPA(self.sno.get(),self.pwd.get())
+        student.getGPA()
+        self.result=student.result
+        if self.result == 'count error!':
+            self.info=Frame(self.root)
+            label=Label(self.info,text=self.result)
+            label.pack()
+            self.info.pack()
+        else:
+            self.treeview=ttk.Treeview(self.root)
+            self.treeview['columns']=('','second','third','fourth')
+            self.treeview.column('second',width=100,anchor="center")
+            self.treeview.column('third',width=100,anchor="center")
+            self.treeview.column('fourth',width=100,anchor="center")
+            self.treeview.heading("second",text="学分")
+            self.treeview.heading("third",text="绩点")
+            self.treeview.heading("fourth",text="成绩")
+            scrollbar=ttk.Scrollbar(self.treeview,orient='vertical',command=self.treeview.yview)
+            self.treeview.configure(yscroll=scrollbar)
+            self.treeview.pack()
+            for eachLine in self.result:
+                if eachLine == self.result[0]:
+                    continue
+                self.treeview.insert('',"end",text=eachLine[3],values=(eachLine[6],eachLine[7],eachLine[8]))
+
+if __name__ == '__main__':
+    GUIGPA()
---
+++ crawlerJXUST.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+"""
+# 程序:crawler for Jiangxi University of Science and Technology
+# 版本:0.1
+# 作者:HFX
+# 日期:GMT 10:46:352014年 4月 19日 (星期六)
+# 语言:Python 3.3
+# 操作:输入学号和密码
+# 功能:输出成绩的绩点
+"""
+
+import urllib.parse
+import urllib.request
+import http.cookiejar
+import html.parser
+import re
+
+import py.utility.encodeToUrl
+
+class GPA(object):
+    def __init__(self,sno,pwd):
+        self.sno=sno
+        self.pwd=pwd
+        self.cookie=http.cookiejar.CookieJar()
+        self.opener=urllib.request.build_opener(urllib.request.HTTPCookieProcessor(self.cookie))
+        header={
+            'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0',
+            'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language':'zh-cn,zh;q=0.8,en-us;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate',
+            'Referer': 'http://218.65.107.173/',
+            'Connection': 'keep-alive'
+            }
+        self.headers=header
+        self.viewstate='dDwxOTI3MTM3Mjk0Ozs+QfR6JETFyd62h+toLFFXl+4IoBw='
+
+    def login(self):
+        postData=urllib.parse.urlencode({
+            'TextBox1':self.sno,
+            'TextBox2':self.pwd,
+            'RadioButtonList1':'学生',
+            '__VIEWSTATE':self.viewstate,
+            'Button1':''
+            }).encode(encoding='gb2312')
+        self.req=urllib.request.Request(
+            url='http://218.65.107.173/default2.aspx',
+            data=postData,
+            headers=self.headers
+            )
+        result=str(self.opener.open(self.req).read(),'gb2312')
+        try:
+            self.url='http://218.65.107.173/%s' % py.utility.encodeToUrl.codeparse(re.findall('href="xscj_gc.aspx\?xh=.*?"',result)[0].replace('href=','').replace('"','').strip(),'gb2312')
+        except IndexError as e:
+            self.result='count error!'
+
+    def main(self):
+        self.req=urllib.request.Request(
+            url=self.url,
+            headers=self.headers
+            )
+        result=str(self.opener.open(self.req).read(),'gb2312')
+        self.viewstate=re.findall('"__VIEWSTATE" value=".*?"',result)[0].replace('"__VIEWSTATE" value=','').replace('"','')
+
+    def GPA(self):
+        postData=urllib.parse.urlencode({
+            '__VIEWSTATE':self.viewstate,
+            'ddlXN':'',
+            'ddlXQ':'',
+            'Button2':'在校学习成绩查询',
+            '__EVENTTARGET':'',
+            '__EVENTARGUMENT':''
+            }).encode(encoding='gb2312')
+        self.req=urllib.request.Request(
+            url=self.url,
+            headers=self.headers,
+            data=postData
+            )
+        result=str(self.opener.open(self.req).read(),'gb2312')
+        reg='<td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td><td>.*</td>'
+        regex=re.compile(reg)
+        result=re.findall(regex,result)
+        for i in range(len(result)):
+            result[i]=result[i].replace('<td>','').split('</td>')
+        self.result=result
+
+    def getGPA(self):
+        self.login()
+        if hasattr(self,'result'):
+            return
+        self.main()
+        self.GPA()
+
+def main():
+    while(True):
+        sno=input('Please enter your student number: ')
+        pwd=input('Please enter your password: ')
+        student=GPA(sno,pwd)
+        student.getGPA()
+        if student.result != 'count error!':
+            for eachLine in student.result:
+                print('%-40s %6s %6s %6s' % (eachLine[3],eachLine[6],eachLine[7],eachLine[8]))
+        else:
+            print(student.result)
+        operator=input('u wanna try again? (Y/N):')
+        if not operator.lower().startswith('y'):
+            break
+
+if __name__ == '__main__':
+    main()
